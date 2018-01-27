6630ab3e59b328248df1aa2e47da2589f28b4dfd
Leuthier <victorleuthiersantos@gmail.com>
2018-01-27 16:28:32 -0300

    estacao ok - o estado/cidade esta bugado, esta pegando de uma estacao a menos

--- dados_estacao.py
+++ dados_estacao.py
@@ -5,6 +5,7 @@ import json
 def pegarestacoes():
     estacoes = {}
     aux = {}
+    qtdade = 0
     
     lat, lng, altitude = 0, 0, 0
     codigo, estado, cidade, nome_estacao = "","","",""
@@ -20,11 +21,12 @@ def pegarestacoes():
             comeco_estacao = linha.find("O")
             fim_estacao = linha.find(" *")        
             nome_estacao = linha[comeco_estacao+2:fim_estacao]
-            estacoes["nome"] = nome_estacao
+##            estacoes['nome'] = nome_estacao
 ##            print("nome estacao",nome_estacao)
 
+
         ##    pegar latitude e longitude da estacao
-        elif ("var point" in linha):
+        if ("var point" in linha):
             lat_lng = linha.strip(",")
             comeco_lat_lng = lat_lng.find("(")
             fim_lat_lng = lat_lng.find(")")
@@ -36,28 +38,35 @@ def pegarestacoes():
 ##            aux['lng'] = lng
 ##            print("latlng",lat_lng)
 
+
         ##  pegar codigo das estacoes
-        elif ("http://www.inmet.gov.br/sonabra/pg_dspDadosCodigo_sim.php?" in linha):
+        if ("http://www.inmet.gov.br/sonabra/pg_dspDadosCodigo_sim.php?" in linha):
             comeco_codigo = linha.find("http://www.inmet.gov.br/sonabra/pg_dspDadosCodigo_sim.php?")
             fim_codigo = linha.find("==")
             codigo = linha[comeco_codigo+len("http://www.inmet.gov.br/sonabra/pg_dspDadosCodigo_sim.php?"):fim_codigo]
 ##            aux['codigo'] = codigo
 ##            print ("codigo",codigo)
 
+
         ##    pegar estado/cidade da estacao
-        elif ("var label = '" in linha):
-            linha = linha.replace("var label = '","")
-            linha = linha.replace("';","")
-            linha = linha.replace(" ","")
-            linha = linha.split("-")
-            estado = linha[0]
-            cidade = linha[1]
+        if ("var label = '" in linha):
+            comeco_endereco = linha.find("'")
+            fim_endereco = linha.find("'; ")
+            endereco = linha[comeco_endereco+1:fim_endereco]
+            endereco = endereco.replace(" ","")
+            endereco = endereco.split("-")
+##            print("endereco:",endereco)
+            estado = endereco[0]
+            cidade = endereco[1]
 ##            aux['estado'] = estado
 ##            aux['cidade'] = cidade
-##            print(estado+"-"+cidade)
+            print("estado-cidade:",estado+"-"+cidade)
+            qtdade += 1
+            print("enderecos",qtdade)
+
             
         ##  pegar altitude
-        elif ("Altitude: " in linha):
+        if ("Altitude: " in linha):
             comeco_altitude = linha.find("Altitude: ")
             fim_altitude = linha.find("metros")
             altitude = linha[comeco_altitude+10:fim_altitude]
@@ -67,12 +76,11 @@ def pegarestacoes():
 
 ##        print("nome_estacao",nome_estacao)
 ##        print("estacoes[nome_estacao]",estacoes[nome_estacao])
-        estacao_nome = nome_estacao
-        estacoes[estacao_nome] = aux
-##        aux.update({'codigo':codigo})
-        print("estacoes",estacoes)
-        print ("est [nome]",estacoes[estacao_nome])
-##        print("aux",aux)
+            estacoes[nome_estacao] = {'codigo':codigo,'lat':lat,'lng':lng,'cidade':cidade,'estado':estado,'altitude':altitude,}
+        
+    print("estacoes['C891']",estacoes['C891'])
+##        print ("est [nome]",estacoes[estacao_nome])
+       
 
 
 ##        saida_json = json.dumps(estacoes[nome_estacao])
