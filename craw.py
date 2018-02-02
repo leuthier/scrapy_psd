771e02e920546bfc0bec9dd2baad6fc2ffba9f2a
viiniciuspaes <viniciusjosepaes@gmail.com>
2018-02-02 19:21:44 -0300

    dados sendo coletados com sucesso

--- craw.py
+++ craw.py
@@ -31,32 +31,70 @@ def spider():
 
 
 def burlarcaptcha():
-    spider()
+    lista_link = spider()
     
     url = "http://www.inmet.gov.br/sonabra/pg_dspDadosCodigo_sim.php?"
 
     x = 1
-    for i in lista_link:
-        print(" -----",x)
+    master_dic = {}
+    for i in lista_link: #TODO: this can be a paramether
+
+        print(" -----", x)
         x += 1
         print(" ----- CODIGO:",i)
         url = "http://www.inmet.gov.br/sonabra/pg_dspDadosCodigo_sim.php?"+i+"=="
-            
+
         form = {
-                'dtaini':'17/01/2018',
+                'dtaini':'17/01/2018', #TODO: get todays date using datetime
                 'dtafim':'17/01/2018',
                 'aleaValue':'NDgyOA==',
                 'aleaNum':'4828'
                 }
-            
+
         encondedForm = urllib.parse.urlencode(form)
 
         head = {
                 'Content-Type': 'application/x-www-form-urlencoded'
                 }
 
-        r = requests.post(url,data=encondedForm,headers=head)
-    print(r.text)
+        r = requests.post(url, data=encondedForm, headers=head)
+        cook = r.cookies.items()
+        cookies = "{}={}".format(cook[0][0], cook[0][1])
+        # print(cookies)
+
+        head2 = {
+            'Referer': url,
+            'Cookie': cookies
+        }
+
+        n = requests.get("http://www.inmet.gov.br/sonabra/pg_downDadosCodigo_sim.php", headers=head2)
+        day_time_list = []
+        data_time = {}
+        response = n.text
+        response = response.replace('////', "-")
+        response = response.replace("\r\n\t\t", "")
+        response = response.replace("\r\n", "")
+        response = response.replace(" ", "")
+        response = response.split("<br>")
+        response = response[:-1:]
+        # for x in response: print(x, len(x.split(",")))
+        if len(response) > 0:
+
+            categories = response.pop(0).split(",")
+            for data_set in response:
+                for data_index in range(len(data_set.split(","))):
+                    data_time[categories[data_index]] = data_set.split(",")[data_index]
+                day_time_list.append(data_time)
+            print(day_time_list)
+            master_dic[i] = day_time_list
+            # print(master_dic)
+        else:
+            print("nenhuma informação disponivel ou parametros de pesquisa invalidos")
+    print(master_dic)
+
+
+
+
         
 
 burlarcaptcha()
