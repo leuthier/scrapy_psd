ea99b3c1fbf2f38c583af36f083b2f778c164ff0
Francisco Queiroga <franciscofqueiroga@gmail.com>
2018-02-06 19:10:08 -0300

    atualização dos scripts
    definição de métodos para credenciais e criação de devices e alguns testes sobre os dados

--- scripts_testes/device_script.py
+++ scripts_testes/device_script.py
@@ -1,13 +1,89 @@
 import requests
 
-headers = {
-    'Content-Type': 'application/json',
-    'Accept': 'application/json',
-    'X-Authorization': 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZW5hbnRAdGhpbmdzYm9hcmQub3JnIiwic2NvcGVzIjpbIlRFTkFOVF9BRE1JTiJdLCJ1c2VySWQiOiJjYzNjZTEwMC05ZmNlLTExZTYtODA4MC04MDgwODA4MDgwODAiLCJlbmFibGVkIjp0cnVlLCJpc1B1YmxpYyI6ZmFsc2UsInRlbmFudElkIjoiY2JhNDRhODAtOWZjZS0xMWU2LTgwODAtODA4MDgwODA4MDgwIiwiY3VzdG9tZXJJZCI6IjEzODE0MDAwLTFkZDItMTFiMi04MDgwLTgwODA4MDgwODA4MCIsImlzcyI6InRoaW5nc2JvYXJkLmlvIiwiaWF0IjoxNTE3NDAzNTE3LCJleHAiOjE1MTc0MDQ0MTd9.R71bPXe3ug8PldTN0a3Ir84eIrLdqYUaagBT963EFPrqf54pnu_fwvq8j01Bq2qsryNbyr9IIAfqT_ZhMpqJcA',
-}
 
-data = '{ \n   "additionalInfo": null, \n   "createdTime": 0, \n   "name": "teste de novo, de novo",\n   "type": "default" \n }'
+def get_token_sys():
+    headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+    }
 
-response = requests.post('http://localhost:8080/api/device', headers=headers, data=data)
+    data = '{"username":"tenant@thingsboard.org", "password":"tenant"}'
 
-print(response.content)
+    response = requests.post('http://localhost:8080/api/auth/login', headers=headers, data=data)
+
+    t = str(response.content)
+    t = t.split('"')
+    token = str(t[3])
+
+    return token
+
+
+def create_device(name):
+    token = 'Bearer ' + get_token_sys()
+    headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'X-Authorization': token,
+    }
+
+    data = '{ \n   "additionalInfo": null, \n   "createdTime": 0, \n   "name": "' + name + '",\n   "type": "default" \n }'
+
+    response = requests.post('http://localhost:8080/api/device', headers=headers, data=data)
+
+    t = str(response.content)
+    t = t.split('"')
+    device_id = t[9]
+    device_name = t[-8]
+    device = {device_id, device_name}
+
+    g = open("devices.txt", "a")
+    g.write(device_name + "\n")
+    g.write(device_id + "\n")
+    g.close()
+
+    return device
+
+
+file = open("devices.txt", "r")
+ls = file.readlines()
+local = ls[1]
+local = local.split(" ")
+local = local[2].split("\n")
+device_id = str(local[0])
+
+
+def get_credentials(device):
+    token = 'Bearer ' + get_token_sys()
+    headers = {
+        'Accept': 'application/json',
+        'X-Authorization': token,
+    }
+
+    response = requests.get('http://localhost:8080/api/device/'+device+'/credentials', headers=headers)
+    t = str(response.content)
+    t = t.split('"')
+    print(t)
+    credential = str(t[-4])
+    g = open("credentials.txt", "a")
+    g.write(credential + "\n")
+    g.close
+
+    return credential
+
+# arquivo antigo para pegar o id do arquivo, com os nomes dos devices e seus ids, método reformulado
+
+# file = open("devices.txt", "r")
+# ls = file.readlines()
+# local = ls[1]
+# local = local.split(" ")
+# local = local[2].split("\n")
+# device_id = str(local[0])
+
+# exemplo para criar devices
+
+# i = 1
+# for i in range(555):
+#    nome = "device " + str(i)
+#    create_device(nome)
+#    i += 1
+#    print("Device created, number: {}".format(i))
---
+++ scripts_testes/tel_test.py
@@ -0,0 +1,59 @@
+import requests
+import time
+
+
+def publish(temperatura, ligado, velocidade):
+    headers = {
+        'Content-Type': 'application/json',
+        'Accept': '*/*',
+        'X-Authorization': 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZW5hbnRAdGhpbmdzYm9hcmQub3JnIiwic2NvcGVzIjpbIlRFTkFOVF9BRE1JTiJdLCJ1c2VySWQiOiI4YTcxMTBmMC0wNjMxLTExZTgtOGEwNS03MWMwMDY3YTg4MjgiLCJlbmFibGVkIjp0cnVlLCJpc1B1YmxpYyI6ZmFsc2UsInRlbmFudElkIjoiOGE2ZmZmODAtMDYzMS0xMWU4LThhMDUtNzFjMDA2N2E4ODI4IiwiY3VzdG9tZXJJZCI6IjEzODE0MDAwLTFkZDItMTFiMi04MDgwLTgwODA4MDgwODA4MCIsImlzcyI6InRoaW5nc2JvYXJkLmlvIiwiaWF0IjoxNTE3OTMzNDY1LCJleHAiOjE1MjY5MzM0NjV9.K-weRFt0sDr-xVbhG0Ll7LV9LDpP8w-oWoXnjjRtXEXmvibL-zUcS1g54QEjuf8AjpmGcj1ncHkOQQDkHANMLw',
+    }
+
+    data = '{ \n   "temperatura": "' + str(temperatura) + '", \n   "ligado": ' + str(
+        ligado) + ', \n   "velocidade": "' + str(velocidade) + '" \n }'
+
+    response = requests.post('http://localhost:8080/api/v1/cHzsIZicvtcFJf6qSP4e/telemetry', headers=headers, data=data)
+    print(response.content)
+
+
+def get_telemetry():
+
+    headers = {
+        'Content-Type': 'application/json',
+        'X-Authorization': 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZW5hbnRAdGhpbmdzYm9hcmQub3JnIiwic2NvcGVzIjpbIlRFTkFOVF9BRE1JTiJdLCJ1c2VySWQiOiI4YTcxMTBmMC0wNjMxLTExZTgtOGEwNS03MWMwMDY3YTg4MjgiLCJlbmFibGVkIjp0cnVlLCJpc1B1YmxpYyI6ZmFsc2UsInRlbmFudElkIjoiOGE2ZmZmODAtMDYzMS0xMWU4LThhMDUtNzFjMDA2N2E4ODI4IiwiY3VzdG9tZXJJZCI6IjEzODE0MDAwLTFkZDItMTFiMi04MDgwLTgwODA4MDgwODA4MCIsImlzcyI6InRoaW5nc2JvYXJkLmlvIiwiaWF0IjoxNTE3OTMzNDY1LCJleHAiOjE1MjY5MzM0NjV9.K-weRFt0sDr-xVbhG0Ll7LV9LDpP8w-oWoXnjjRtXEXmvibL-zUcS1g54QEjuf8AjpmGcj1ncHkOQQDkHANMLw',
+    }
+
+    params = (
+        ('keys', 'temperatura,velocidade,ligado'),
+        ('startTs', ''),
+        ('endTs', ''),
+        ('interval', ''),
+        ('limit', ''),
+        ('agg', ''),
+    )
+
+    response = requests.get(
+        'http://localhost:8080/api/plugins/telemetry/DEVICE/5445f210-08d0-11e8-9bfd-71c0067a8828/values/timeseries',
+        headers=headers, params=params)
+
+#     método antigo de request, sem passagem dos parâmetros, para teste direto no URL
+#     response = requests.get('http://localhost:8080/api/plugins/telemetry/DEVICE/ac8e6020-ae99-11e6-b9bd-2b15845ada4e/values/timeseries?keys=gas,temperature', headers=headers)
+    print(response.content)
+    return response.content
+
+# teste do publish
+# i = 0
+# t = 300
+# v = 400
+# for i in range(100):
+
+#    if i % 2 == 0:
+#        li = "true"
+#    else:
+#        li = "false"
+
+#    publish(t, li, v)
+#    time.sleep(4)
+#    t += 5
+#    v -= 1
+#    i += 1
